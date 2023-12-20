1a0e0cdfc1a786cc10361463b4b3d36687def5db
TeooL <144486916+TeooL@users.noreply.github.com>
2023-12-20 15:34:20 -0500

    Add files via upload

--- Algorithm.java
+++ Algorithm.java
@@ -50,4 +50,72 @@ public class Algorithm {
         }
         return longest;
     }
+    public int largestAbsDifference(int[] setOne, int[] setTwo){
+        int largest = Math.abs(setOne[0] - setTwo[0]);
+        for (int i = 1; i <setOne.length;i++){
+            if (Math.abs(setOne[i] - setTwo[i]) > largest){
+                largest = Math.abs(setOne[i] - setTwo[i]);
+            }
+        }
+        return largest;
+    }
+    public double mean(double[] array){
+        double total = 0;
+        for (double i: array){
+            total += i;
+        }
+        return total / array.length;
+    }
+    public void reverse(int[] numSet){
+        for (int i = 0;i<numSet.length/2;i++){
+            int temp = numSet[i];
+            numSet[i] = numSet[numSet.length - 1 - i];
+            numSet[numSet.length - 1 -i] = temp;
+        }
+    }
+    public boolean haveConsecutive(int[] numSet){
+        int first = 0;
+        int second = 0;
+        for (int i = 0; i < numSet.length - 2;i++){
+            first = numSet[i];
+            second = numSet[i+1];
+            if (first == second) return true;
+        }
+        return false;
+    }
+    public int firstNegative(int[] numSet){
+        int index = 0;
+        for (int i = 0; i < numSet.length;i++){
+            if (numSet[i] < 0){
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+    public boolean equivalentValues(int[] numSet){
+        for (int i = 0; i < numSet.length;i++){
+            int temp = numSet[i];
+            for (int x = i + 1; x < numSet.length;i++){
+                if (numSet[x]==temp){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public int longestStreak(int[] numSet, int target){ // Finish This At Home
+        int longStreak = 0;
+        int streak = 0;
+        for (int i = 0;i<numSet.length;i++){
+            while (numSet[i]== target && numSet[i+1]==target){
+                streak++;
+            }
+        }
+        return longStreak;
+    }
+    public String lastAlphabetically(String[] words){
+        String last = "";
+        return last;
+    }
 }
--- Main.java
+++ Main.java
@@ -9,7 +9,16 @@ public class Main {
         a.switchElements(new int[]{9,10,11,12,13},2,4);
         System.out.println(a.returnMax(new int[]{3,2,1}));
         System.out.println(a.returnMin(new int[]{3,2,1}));
-        System.out.println(a.returnLongest(new String[]{"hello,","world","!"}));
         System.out.println(a.returnShortest(new String[]{"hello,","world","!"}));
+        System.out.println(a.returnLongest(new String[]{"hello,","world","!"}));
+        System.out.println(a.largestAbsDifference(new int[]{2,3,4}, new int[]{-1,-2,-3}));
+        System.out.println(a.mean(new double[]{2.0,3.0,4.0,5.0}));
+        int[] case1 = {1,2,3,4};
+        int[] case2 = {5,6,7,7,9,10};
+        int[] case3 = {1,2,3,3,4};
+        a.reverse(case1);
+        System.out.println(a.haveConsecutive(case3));
+
+
     }
 }
